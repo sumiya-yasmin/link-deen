2a2706d59cd4c8f227abe47d03e9fdf7461d8757
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-04-22 00:15:06 +0600

    Add basic form for signup

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,5 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export default function AuthLayout() {
     return (
-      <div>rootLayout</div>
+      <div>
+        <Outlet />
+      </div>
     )
   }
--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,6 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function RootLayout() {
   return (
-    <div>rootLayout</div>
+    <div>
+        <Outlet />
+    </div>
   )
 }
 
