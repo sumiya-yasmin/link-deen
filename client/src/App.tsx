bfad573418cd2bc239dae9a208345565941b0c57
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-04-21 09:26:45 +0600

    install tailwind and shadeCn in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+
+import { Button } from './components/ui/button';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <Button>Click me</Button>
+        </div>
+      )
 }
 
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom/client';
 import App from './App';
+import './globals.css';
 
 ReactDom.createRoot(document.getElementById('root')!).render(<App />);
