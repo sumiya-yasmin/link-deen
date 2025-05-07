7aaaa01dfc0d7b63099b3f4e62b0412accf977e2
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-05-07 21:01:56 +0600

    Configure environment variables with dotenv

---
+++ server/src/config/index.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config()
+export const config = {
+    PORT: process.env.PORT,
+    db: {
+      MONGO_URI: process.env.MongoURI,
+    //   Mongo_DBName: process.env.dbName,
+    },
+    // CORS: {
+    //   ORIGIN: process.env.CORS_ORIGIN
+    // }
+  };
