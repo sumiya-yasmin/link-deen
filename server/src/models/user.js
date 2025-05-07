a2ae4b8785d4a683553806341ffb10563469cb28
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-05-08 00:04:14 +0600

    hash password using bcrypt

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,9 +1,22 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 },{timestamps: true});
+
+//hash before save
+userSchema.pre('save', async function () {
+    if(!this.isModified('password')) return;
+    this.password= await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (candidate) {
+   return await bcrypt.compare(candidate, this.password);
+    
+}
 const User = mongoose.model('User', userSchema);
 export default User;
--- server/src/validations/auth.js
+++ server/src/validations/auth.js
@@ -5,4 +5,8 @@ export const signupSchema = z.object({
   username: z.string().min(3, 'Username must be at least 3 characters'),
   email: z.string().email('Invalid email'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
+  confirmPassword: z.string()
+}).refine(data => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"]
 });
