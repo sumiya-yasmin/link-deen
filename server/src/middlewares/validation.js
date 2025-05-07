fb5fd10ff02f5fa53b6626ea2833633eb8275b44
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-05-07 23:39:09 +0600

    add zod validation middleware

---
+++ server/src/middlewares/validation.js
@@ -0,0 +1,8 @@
+const validatePayload = (schema) => {
+  return (req, res, next) => {
+    const payload = req.body;
+    schema.parse(payload);
+    next();
+  };
+};
+export default validatePayload;
