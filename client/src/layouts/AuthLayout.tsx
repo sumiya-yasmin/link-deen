ac34ff8f7e0e12cd089e1a58c99813a1e5bed41d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-05-08 19:29:51 +0600

    configure toaster for showing signup success message and redirect to sign-in

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 export default function AuthLayout() {
   return (
     <>
-    <div className="flex">
-      <section className="flex flex-col flex-1 justify-baseline  items-center py-8">
-      <div className="flex flex-col items-center justify-center gap-4 mb-8">
+      <div className="flex">
+        <section className="flex flex-col flex-1 justify-baseline  items-center py-8">
+          <div className="flex flex-col items-center justify-center gap-4 mb-8">
             <div className="rounded-full bg-amber-100 p-3">
               <img src="/assets/logo.PNG" className="w-16 h-16" alt="Logo" />
             </div>
@@ -13,18 +14,20 @@ export default function AuthLayout() {
               Connect through Deen
             </h2>
             <p className="text-slate-600 text-center max-w-sm">
-              Join our community to connect with like-minded individuals on your spiritual journey.
+              Join our community to connect with like-minded individuals on your
+              spiritual journey.
             </p>
           </div>
-        <Outlet />
-      </section>
-      <div className="hidden xl:block xl:w-1/2 h-full ">
-        <img src="/assets/hero.png" className="w-full object-cover h-full" />
+          <Outlet />
+        </section>
+        <div className="hidden xl:block xl:w-1/2 h-full ">
+          <img src="/assets/hero.png" className="w-full object-cover h-full" />
+        </div>
       </div>
-    </div>
-    <footer className="text-center p-6 text-xs text-slate-400">
-          &copy; {new Date().getFullYear()} LinkDeen. All rights reserved.
-        </footer>
+      <footer className="text-center p-6 text-xs text-slate-400">
+        &copy; {new Date().getFullYear()} LinkDeen. All rights reserved.
+      </footer>
+      <Toaster position="top-right" />
     </>
   );
 }
--- client/src/pages/auth/SignupPage.tsx
+++ client/src/pages/auth/SignupPage.tsx
@@ -12,12 +12,16 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { signupFormSchema } from '@/lib/validation';
-import { Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import API from '@/lib/axios';
 import { AxiosError } from 'axios';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 const Signup = () => {
-  const isLoading = false;
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
   const form = useForm<z.infer<typeof signupFormSchema>>({
     resolver: zodResolver(signupFormSchema),
     defaultValues: {
@@ -28,102 +32,126 @@ const Signup = () => {
       confirmPassword: '',
     },
   });
-  const onSubmit = async (data: z.infer<typeof signupFormSchema>):Promise<void> => {
+  const onSubmit = async (
+    data: z.infer<typeof signupFormSchema>
+  ): Promise<void> => {
+    setIsLoading(true);
     try {
       const response = await API.post('/auth/signup', data);
       console.log('User created:', response.data);
+      toast.success('Signup successful! Redirecting to login...');
+      setTimeout(() => {
+        navigate('/sign-in');
+      }, 2000);
     } catch (error) {
       if (error instanceof AxiosError) {
-        console.error('Signup failed:', error.response?.data.message || error.message);
+        console.error(
+          'Signup failed:',
+          error.response?.data.message || error.message
+        );
       } else {
         console.error('Unexpected error:', error);
       }
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
   return (
     <>
-    <div className="w-1/2 flex flex-col justify-center">
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className=" text-center space-y-6">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input type='text' placeholder='Name' className='shad-input' {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="username" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <Input placeholder="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className='w-full flex justify-center py-2.5 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-amber-600 hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 transition-colors'>{isLoading?(
-        <div>Sigining Up...</div>
-      ): 'Sign Up'}</Button>
-      </form>
-      
-    </Form>
-    <div className="text-center text-sm text-slate-500 mt-4">
-            Already have an account?{" "}
-            <Link to="/signin" className="text-amber-600 hover:text-amber-500 font-medium">
-              Sign In
-            </Link>
-          </div>
-    
-    </div>
+      <div className="w-1/2 flex flex-col justify-center">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className=" text-center space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Name"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="w-full flex justify-center py-2.5 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-amber-600 hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-500 transition-colors"
+            >
+              {isLoading ? <div>Sigining Up...</div> : 'Sign Up'}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center text-sm text-slate-500 mt-4">
+          Already have an account?{' '}
+          <Link
+            to="/sign-in"
+            className="text-amber-600 hover:text-amber-500 font-medium"
+          >
+            Sign In
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
