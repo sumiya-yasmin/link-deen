c64e6a634bc57337133c2a4cd7b7f178a5649aa4
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-05-21 21:21:48 +0600

    Add top navigation bar for mobile devces

--- client/src/components/shared/Topbar.tsx
+++ client/src/components/shared/Topbar.tsx
@@ -1,24 +1,30 @@
-  import { Link } from 'react-router-dom';
-  import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-  } from '@/components/ui/icons/avatar';
+  import { useAuth } from '@/context/AuthContext';
+import { Link } from 'react-router-dom';
+import { Button } from '../ui/button';
+  // import {
+  //   Avatar,
+  //   AvatarFallback,
+  //   AvatarImage,
+  // } from '@/components/ui/icons/avatar';
 
   function Topbar() {
+    const { handleSignout } = useAuth();
     return (
-      <section className=''>
+      <section className='sticky md:hidden top-0 z-50  bg-dark-2 w-full'>
           <div className='flex justify-between items-center'>
-        <Link to="/" className="flex gap=3 items-center">
-          <img src="/assets/logo.PNG" className="rounded-full h-16 w-16" />
+        <Link to="/" className="flex gap=3 items-center p-2">
+          <img src="/assets/logo5.png" className="" width={130} height={325} />
         </Link>
-        <div>
-          <div>Logout</div>
+        <div className='flex gap-3'>
+          <Button variant='ghost' onClick={()=>handleSignout()} className='bg-gray-700'>
+          <img src='/assets/out.png' alt="profile" className='h-8 w-8 rounded-full' />
+          </Button>
           <Link to="/profile">
-          <Avatar>
+          <img src='/assets/profile-placeholder.png' alt="profile" className='h-8 w-8 rounded-full' />
+          {/* <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          </Avatar> */}
           </Link>
         </div>
         </div>
