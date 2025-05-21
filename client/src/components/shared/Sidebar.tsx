0b31d5cb380eb235d86ba5cd3e7d8a91507f70ec
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-05-21 21:22:20 +0600

    Add side navigation bar

--- client/src/components/shared/Sidebar.tsx
+++ client/src/components/shared/Sidebar.tsx
@@ -1,7 +1,63 @@
+import { Link, useLocation } from 'react-router-dom';
+import {
+  Home,
+  Users,
+  Settings,
+  PlusSquare,
+  Files,
+  Telescope,
+} from 'lucide-react';
+
+import { useAuth } from '@/context/AuthContext';
+
+const sideNavItems = [
+  // { icon: '/assets/react.svg', label:'Profile', path: '/profile' },
+
+  { icon: Home, label: 'Home', path: '/' },
+  { icon: PlusSquare, label: 'Create Post', path: '/create' },
+  { icon: Telescope, label: 'Explore', path: '/explore' },
+  { icon: Users, label: 'Friends', path: '/people' },
+  { icon: Files, label: 'Dua Board', path: '/blogs' },
+  { icon: Settings, label: 'Settings', path: '/settings' },
+];
+
 function Sidebar() {
+  const { user } = useAuth();
+  const location = useLocation()
   return (
-    <div>Sidebar</div>
-  )
+    <nav className="hidden md:flex bg-dark-2 px-6 py-2 min-w-[270px] flex-col justify-between h-full">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex items-center ">
+          <img src="/assets/logo5.png" className="" width={170} height={30} />
+        </Link>
+        <Link to="/profile" className='flex items-center gap-2'>
+          <img
+            src="/assets/profile-placeholder.png"
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className='flex flex-col'>
+            <p className='text-[18px] font-bold leading-[140%]'>{user?.name}</p>
+            <p className='text-light-3 text-[14px] font-normal leading-[140%]'>@{user?.username}</p>
+          </div>
+        </Link>
+        <ul className="space-y-6">
+          {sideNavItems.map((item) => {
+            const Icon = item.icon;
+            const isActive = location.pathname === item.path;
+            return (
+              <li key={item.label} className={`group p-2 ${isActive? 'bg-[#CD7F32]':''} hover:bg-[#CD7F32] rounded-md`}>
+                <Link to={item.path} className="flex items-center gap-2">
+                  <Icon className={`w-6 h-6 text-[#CD7F32]  ${isActive? 'text-[#ffffff]':''} group-hover:text-[#ffffff]`} />
+                  <span className="ml-3">{item.label}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
