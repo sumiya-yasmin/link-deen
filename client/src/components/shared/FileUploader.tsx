8a95316ba9e52bf9779a5c1f6f5c0e51dae39720
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-03 23:07:04 +0600

    Add reset logic in file uploader

--- client/src/components/shared/FileUploader.tsx
+++ client/src/components/shared/FileUploader.tsx
@@ -1,27 +1,36 @@
 import { ImageUp } from 'lucide-react'
-import React, {useCallback, useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import {FileWithPath,useDropzone} from 'react-dropzone'
 import { Button } from '../ui/button'
 
 type FileUploaderProps ={
     fieldChange: (FILES: File[]) =>void;
     mediaUrl: string;
+    setClearFilePreview: React.Dispatch<React.SetStateAction<() => void>>;
 }
 
-function FileUploader({fieldChange, mediaUrl}: FileUploaderProps) {
+function FileUploader({fieldChange, mediaUrl, setClearFilePreview}: FileUploaderProps) {
     const [file, setFile] =useState<File[]>([])
     const [fileUrl, setFileUrl] = useState('')
     const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
       setFile(acceptedFiles);
       fieldChange(acceptedFiles);
       setFileUrl(URL.createObjectURL(acceptedFiles[0]));
-    }, [file])
+    }, [fieldChange])
     const {getRootProps, getInputProps} = useDropzone({
         onDrop,
         accept:{
             'image/*': ['.png', '.jpeg', '.jpg', '.svg']
         }
     })
+
+   useEffect(() => {
+  setClearFilePreview(() => () => {
+    setFile([]);
+    setFileUrl('');
+  });
+}, [setClearFilePreview]);
+
   return (
      <div {...getRootProps()} className='flex flex-col bg-dark-3 rounded-xl cursor-pointer items-center justify-center'>
       <input {...getInputProps()} className='cursor-pointer'/>
@@ -30,9 +39,9 @@ function FileUploader({fieldChange, mediaUrl}: FileUploaderProps) {
             <>
 <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
     <img
-    src={fileUrl} 
+    src={fileUrl || mediaUrl} 
     alt='image'
-    className='h-80 lg:h-[480px] w-full rounded-[24px] object-cover object-top'/>
+    className='h-80 lg:h-[480px] lg:object-contain w-full rounded-[24px] object-cover object-top'/>
 </div>
 <p className='text-light-4 text-center small-regular w-full p-4 border-t border-t-dark-4'>Click or Drag photo to replace</p>
 </>
@@ -41,7 +50,7 @@ function FileUploader({fieldChange, mediaUrl}: FileUploaderProps) {
              <ImageUp className='w-24 h-24 text-[#CD7F32]' />
              <h3 className='text-light-2 text-[16px] font-medium leading-[140%] mb-2 mt-6'>Drag Photo here</h3>
              <p className='text-light-4 text-[14px] font-normal leading-[140%] mb-6'>SVG, PNG, JPG</p>
-             <Button className='h-12 bg-dark-4 px-5 text-light-1 flex gap-2'>Select from device</Button>
+             <Button  type="button" className='h-12 bg-dark-4 px-5 text-light-1 flex gap-2'>Select from device</Button>
             </div> 
         )
       }
