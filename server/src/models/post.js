0c355fa0eeb4c596965cf0f83dc662236436485a
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-04 15:39:31 +0600

    Add post mongoose model

---
+++ server/src/models/post.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+ 
+    caption: { type: String, maxlength: 2200, trim: true, },
+    imageUrl: { type: String }, 
+    location: { type: String, maxlength:100, trtim:true, },
+    tags: [{ type: String, trim:true, }],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+ comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    text: {
+      type: String,
+      required: true,
+      maxlength: 500,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }],
+},
+
+  { timestamps: true }
+)
+
+PostSchema.pre('save',function(next){
+    if(!this.caption && !this.imageUrl){
+        const error = new Error('The post can not be empty');
+        error.name = 'Validation Error'
+        return next(error)
+    };
+    next();
+})
+export const Post = mongoose.model('Post', PostSchema);
