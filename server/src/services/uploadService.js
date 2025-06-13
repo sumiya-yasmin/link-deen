eca46cd001d70c87ebae1ae48817934f59907c49
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-13 23:37:30 +0600

    Add delete image services

--- server/src/services/uploadService.js
+++ server/src/services/uploadService.js
@@ -1,21 +1,39 @@
 import { cloudinary } from '../config/cloudinaryConfig.js';
-class uploadService{
-     async uploadImage(file) {
+
+class UploadService {
+  async uploadImage(file) {
     return new Promise((resolve, reject) => {
-      cloudinary.uploader.upload_stream(
-        {
-          resource_type: 'image',     
-          folder: 'posts',           
-          transformation: [           
-            { quality: 'auto' },
-            { fetch_format: 'auto' }
-          ]
-        },
-        (error, result) => {
-          if (error) reject(error);      
-          else resolve(result.secure_url);  
-        }
-      ).end(file.buffer); 
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: 'image',
+            folder: 'posts',
+            transformation: [{ quality: 'auto' }, { fetch_format: 'auto' }],
+          },
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result.secure_url);
+          }
+        )
+        .end(file.buffer);
     });
   }
+
+  async deleteImage(imageUrl) {
+    try {
+      const publicId = this.extractPublicId(imageUrl);
+      await cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+      console.error('Failed to delete image:', error.message);
+    }
+  }
+
+  extractPublicId(imageUrl) {
+    const parts = imageUrl.split('/');
+    const filename = parts[parts.length - 1];
+    const publicId = `posts/${filename.split('.')[0]}`;
+    return publicId;
+  }
 }
+
+export default new UploadService();
