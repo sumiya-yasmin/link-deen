cb6a48cadb3698af1e8a61dae01a02318ca2c8b4
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-13 23:11:33 +0600

    configure cloudinary in app

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 app.use(cors(config.CORS));
 
 connectDB();
+configureCloudinary();
 configureRouter(app);
 app.listen(port, ()=>{
     console.log(`Server running on http://localhost:${port}`);
