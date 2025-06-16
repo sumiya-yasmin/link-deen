6adf2814e0235eb995574cbd2ce71baf41b955c0
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-16 22:19:59 +0600

    Add getRecentPost query hook

--- client/src/hooks/usePostApis.tsx
+++ client/src/hooks/usePostApis.tsx
@@ -1,22 +1,37 @@
-import { createPost } from "@/api/posts"
-import { QUERY_KEYS } from "@/constants/queryKeys";
-import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { AxiosError } from "axios";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { createPost, getRecentPosts } from '@/api/posts';
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
-export const useCreatePost = () =>{
-    const queryClient = useQueryClient();
-        const navigate = useNavigate();
-    return useMutation({
-        mutationFn: createPost,
-        onSuccess: ()=>{
-            toast.success('Post created successfully');
-            queryClient.invalidateQueries({queryKey: [QUERY_KEYS.GET_RECENT_POSTS]});
-            navigate('/');
-        },
-        onError: (error: AxiosError<{ message?: string }>)=>{
-            toast.error(error.response?.data.message || 'Post creation failed');
-        }
-    })
-}
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: createPost,
+    onSuccess: () => {
+      toast.success('Post created successfully');
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      navigate('/');
+    },
+    onError: (error: AxiosError<{ message?: string }>) => {
+      toast.error(error.response?.data.message || 'Post creation failed');
+    },
+  });
+};
+
+export const useGetRecentPosts = () => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+    initialPageParam: null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+};
