149fc5969fd9c72a8e7f3209d81ce97460daab2d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-22 23:52:25 +0600

    Add get user posts api in frontend

--- client/src/api/profile.ts
+++ client/src/api/profile.ts
@@ -1,6 +1,22 @@
-import API from "@/lib/axios"
+import API from '@/lib/axios';
+import { PaginatedPostsResponse, User, UserProfile } from '@/types';
 
-export const getProfileById = async(profileId: string) =>{
-   const response = await API.get(`/profile/${profileId}`);
-   return response.data;
-}
+export const getProfileById = async (profileId: string): Promise<UserProfile> => {
+  const response = await API.get(`/profile/${profileId}`);
+  return response.data as UserProfile;
+};
+
+export const getUserPosts = async ({
+  userId,
+  page = 1,
+  limit = 10,
+}: {
+  userId: string;
+  page?: number;
+  limit?: number;
+}): Promise<PaginatedPostsResponse> => {
+  const response = await API.get(`/post/user/${userId}`,{
+    params: { page, limit },
+});
+  return response.data;
+};
