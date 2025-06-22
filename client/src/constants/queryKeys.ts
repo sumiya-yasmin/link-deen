72c511bd2b8a51017b3f89fc459518f2aa649834
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-06-22 23:53:24 +0600

    Add query for get user post api

--- client/src/constants/queryKeys.ts
+++ client/src/constants/queryKeys.ts
@@ -1,6 +1,7 @@
 export const QUERY_KEYS = {
   GET_PROFILE_BY_ID: 'get-profile-by-id',
-  
+  GET_USER_POSTS: 'get-user-posts',
+
   GET_RECENT_POSTS: 'get-recent-posts',
   GET_POST_BY_ID: 'getPostById',
   GET_COMMENTS: 'getComments',
--- client/src/hooks/useProfileApi.tsx
+++ client/src/hooks/useProfileApi.tsx
@@ -1,6 +1,6 @@
-import { getProfileById } from '@/api/profile';
+import { getProfileById, getUserPosts } from '@/api/profile';
 import { QUERY_KEYS } from '@/constants/queryKeys';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 export const useGetProfileById = (profileId: string, enabled = true) => {
   return useQuery({
@@ -8,4 +8,17 @@ export const useGetProfileById = (profileId: string, enabled = true) => {
     queryFn: () => getProfileById(profileId),
     enabled: !!profileId && enabled,
   });
+};
+
+export const useGetUserPosts = (userId: string, limit: number = 10) => {
+  return useInfiniteQuery({
+  queryKey: [QUERY_KEYS.GET_USER_POSTS, userId, limit],
+  queryFn: ({pageParam =1})=> getUserPosts({ userId, page: pageParam, limit }),
+  enabled: !!userId,
+  initialPageParam: 1, 
+  getNextPageParam: (lastPage)=>{
+    return lastPage.pagination.hasNextPage?  lastPage.pagination.currentPage + 1 
+        : undefined;
+  },
+  })
 };
