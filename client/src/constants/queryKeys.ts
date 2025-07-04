cdb578887ff20b21e8f1b5d1f91f3f261eee2c17
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-07-04 20:32:26 +0600

    Add query key for popular posts

--- client/src/constants/queryKeys.ts
+++ client/src/constants/queryKeys.ts
@@ -1,8 +1,10 @@
 export const QUERY_KEYS = {
   GET_PROFILE_BY_ID: 'get-profile-by-id',
-  GET_USER_POSTS: 'get-user-posts',
-
-  GET_RECENT_POSTS: 'get-recent-posts',
   GET_POST_BY_ID: 'getPostById',
+
+  GET_USER_POSTS: 'get-user-posts',
+  GET_RECENT_POSTS: 'get-recent-posts',
+  GET_POPULAR_POSTS: 'get-popular-posts',
+  
   GET_COMMENTS: 'getComments',
 };
