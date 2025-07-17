1f7ee52421b144e240016c3d87854f94b37b08d1
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-07-17 23:36:01 +0600

    add follow and unfollow services

--- server/src/services/getUserProfileServices.js
+++ server/src/services/getUserProfileServices.js
@@ -29,3 +29,50 @@ export const updateUserProfileService = async (id, updates) => {
   ).select('-password -refreshToken');
   return updatedProfile;
 };
+
+
+export const followUserService = async (currentUserId, targetUserId) => {
+  if (currentUserId === targetUserId) {
+    throw new Error("You can't follow yourself");
+  }
+
+  const currentUser = await User.findById(currentUserId);
+  const targetUser = await User.findById(targetUserId);
+
+  if (!currentUser || !targetUser) {
+    throw new NotFoundError("User not found");
+  }
+
+  if (targetUser.followers.includes(currentUserId)) {
+    throw new Error("Already following this user");
+  }
+
+  targetUser.followers.push(currentUserId);
+  currentUser.following.push(targetUserId);
+
+  await targetUser.save();
+  await currentUser.save();
+    return { currentUser, targetUser };
+};
+
+
+export const unfollowUserService = async (currentUserId, targetUserId) => {
+  const currentUser = await User.findById(currentUserId);
+  const targetUser = await User.findById(targetUserId);
+
+  if (!currentUser || !targetUser) {
+    throw new Error("User not found");
+  }
+
+  targetUser.followers = targetUser.followers.filter(
+    (id) => id.toString() !== currentUserId
+  );
+
+  currentUser.following = currentUser.following.filter(
+    (id) => id.toString() !== targetUserId
+  );
+
+  await targetUser.save();
+  await currentUser.save();
+    return { currentUser, targetUser };
+};
