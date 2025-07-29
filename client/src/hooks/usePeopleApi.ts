3dcd6df59a01c9080ec5db567976798f3c574834
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-07-29 23:55:34 +0600

    Add suggested, search people query hook

---
+++ client/src/hooks/usePeopleApi.ts
@@ -0,0 +1,18 @@
+import { getSearchedUsers, getSuggestedUsers } from "@/api/people"
+import { QUERY_KEYS } from "@/constants/queryKeys"
+import { useQuery } from "@tanstack/react-query"
+
+export const useSuggestedPeople = (page: number=1, limit: number=10) => {
+   return useQuery({
+     queryKey: [QUERY_KEYS.GET_SUGGESTED_USERS, page, limit],
+     queryFn: () => getSuggestedUsers(page, limit),
+    })
+}
+
+export const useSearchedPeople = (que: string, page: number=1, limit: number=10) => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_SEARCHED_USERS, que, page, limit],
+        queryFn: () =>getSearchedUsers({que, page, limit}),
+        enabled: !!que,
+    })
+}
