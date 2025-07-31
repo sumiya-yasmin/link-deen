2d11108901fd5a97469fbad377f702b689188916
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-07-31 21:16:29 +0600

    Invalidate suggested,searched people queries for signup:create new account

--- client/src/hooks/useSignUp.tsx
+++ client/src/hooks/useSignUp.tsx
@@ -1,18 +1,26 @@
 import { signUpMutation } from '@/api/auth';
-import { useMutation } from '@tanstack/react-query';
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
-export const useSignup = () =>{
-    const navigate = useNavigate()
-    return useMutation({
-        mutationFn: signUpMutation,
-        onSuccess: ()=>{
-            toast.success('Signup successful! Redirecting to login...');
-            navigate('/auth/sign-in');
-        },
-        onError: (error: AxiosError<{ message?: string }>)=>{
-            toast.error(error.response?.data.message || 'Signup failed');
-        }
-    })
-}
+export const useSignup = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: signUpMutation,
+    onSuccess: () => {
+      toast.success('Signup successful! Redirecting to login...');
+      navigate('/auth/sign-in');
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_SUGGESTED_USERS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_SEARCHED_USERS],
+      });
+    },
+    onError: (error: AxiosError<{ message?: string }>) => {
+      toast.error(error.response?.data.message || 'Signup failed');
+    },
+  });
+};
