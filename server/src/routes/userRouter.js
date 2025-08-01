0e581aaca897ec44fcc54c10b52d5d24a81f0f70
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-01 22:02:47 +0600

    Add getFollower, getFollowing router

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { authenticate } from '../middlewares/Authenticate.js';
-import { getSearchedUsers, getSuggestedUsers } from '../controllers/getUserProfileController.js';
+import { getFollowers, getFollowing, getSearchedUsers, getSuggestedUsers } from '../controllers/getUserProfileController.js';
 const userRouter = express.Router();
 userRouter.get('/suggested', authenticate, getSuggestedUsers);
 userRouter.get('/search', authenticate, getSearchedUsers);
+userRouter.get('/:id/followers', authenticate, getFollowers);
+userRouter.get('/:id/following', authenticate, getFollowing);
 export default userRouter;
