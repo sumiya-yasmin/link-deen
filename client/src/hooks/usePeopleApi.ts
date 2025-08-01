22618318c5bb0beb22d1c4097b8e0769b284852c
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-01 22:04:10 +0600

    Add query hook for getFollower, getFollowing api

--- client/src/hooks/usePeopleApi.ts
+++ client/src/hooks/usePeopleApi.ts
@@ -1,4 +1,9 @@
-import { getSearchedUsers, getSuggestedUsers } from '@/api/people';
+import {
+  getfollowers,
+  getfollowing,
+  getSearchedUsers,
+  getSuggestedUsers,
+} from '@/api/people';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
@@ -22,19 +27,57 @@ export const useSuggestedPeople = (limit: number = 10) => {
 export const useSearchedPeople = (
   que: string,
   page: number = 1,
-  limit: number = 10,
+  limit: number = 10
 ) => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_SEARCHED_USERS, que, page, limit],
-    queryFn: ({pageParam = 1}) => getSearchedUsers({ que, page: pageParam, limit }),
+    queryFn: ({ pageParam = 1 }) =>
+      getSearchedUsers({ que, page: pageParam, limit }),
     getNextPageParam: (lastPage) => {
-        if(lastPage.currentPage < lastPage.totalPages){
-           return lastPage.currentPage + 1; 
-        }
-        return undefined;
+      if (lastPage.currentPage < lastPage.totalPages) {
+        return lastPage.currentPage + 1;
+      }
+      return undefined;
     },
     initialPageParam: 1,
     enabled: !!que,
+    retry: 2,
+    staleTime: 5 * 60 * 1000,
+    placeholderData: (previousData) => previousData,
+  });
+};
+
+export const useFollowers = (userId: string, limit: number = 10) => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_FOLLOWERS,userId, limit],
+    queryFn: ({ pageParam = 1 }) => getfollowers(userId, pageParam || 1, limit),
+    getNextPageParam: (lastPage) => {
+      if (lastPage.currentPage < lastPage.totalPages) {
+        return lastPage.currentPage + 1;
+      }
+      return undefined;
+    },
+    initialPageParam: 1,
+    retry: 2,
+    staleTime: 5 * 60 * 1000,
+    placeholderData: (previousData) => previousData,
+  });
+};
+
+export const useFollowing = (userId: string, limit: number = 10) => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_FOLLOWING,userId, limit],
+    queryFn: ({ pageParam = 1 }) =>{
+         console.log('Fetching followers, pageParam:', pageParam);
+       return  getfollowing(userId, pageParam || 1, limit);
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.currentPage < lastPage.totalPages) {
+        return lastPage.currentPage + 1;
+      }
+      return undefined;
+    },
+    initialPageParam: 1,
     retry: 2,
     staleTime: 5 * 60 * 1000,
     placeholderData: (previousData) => previousData,
