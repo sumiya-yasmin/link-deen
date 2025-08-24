578828696e405147baa6d8c91fd336b240134f75
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-08-24 12:24:40 +0600

    Update AuthLayout ui

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -5,26 +5,32 @@ export default function AuthLayout() {
   return (
     <>
       <div className="flex">
-        <section className="flex flex-col flex-1 justify-baseline  items-center py-8">
+        <section className="flex flex-col flex-1 justify-baseline  items-center py-4">
           <div className="flex flex-col items-center justify-center gap-4 mb-8">
-            <div className="rounded-full bg-amber-100 p-3">
-              <img src="/assets/logo.PNG" className="w-16 h-16" alt="Logo" />
+            <div className="rounded-full bg-amber-100 ">
+              <img src="/assets/logoAuth.png" className="w-20 h-20 bg-black" alt="Logo" />
             </div>
-            <h2 className="font-bold text-3xl text-amber-600">
-              Connect through Deen
-            </h2>
-            <p className="text-slate-600 text-center max-w-sm">
-              Join our community to connect with like-minded individuals on your
-              spiritual journey.
-            </p>
+                          <div className="space-y-2">
+                <h1 className="text-3xl font-bold bg-[#a67b37] bg-clip-text text-transparent text-center">
+                  LinkDeen
+                </h1>
+                <h2 className="text-xl font-semibold text-gray-800 text-center">
+                  Connect through Deen
+                </h2>
+                <p className="text-gray-600 max-w-sm mx-auto text-center">
+                  Join our community to connect with like-minded individuals on your spiritual journey.
+                </p>
+              </div>
+
           </div>
           <Outlet />
         </section>
         <div className="hidden xl:block xl:w-1/2 h-full ">
-          <img src="/assets/hero.png" className="w-full object-cover h-full" />
+          <img src="/assets/hero4.png" className=" object-cover h-screen fixed right-0 w-3/7" />
         </div>
       </div>
-      <footer className="text-center p-6 text-xs text-slate-400">
+      <hr className='text-gray-800 w-[40%] ml-16'></hr>
+      <footer className="text-left py-6 ml-54 text-xs text-slate-400">
         &copy; {new Date().getFullYear()} LinkDeen. All rights reserved.
       </footer>
 
